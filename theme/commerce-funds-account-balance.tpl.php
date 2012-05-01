3cb04002b001b56f07895ce5bb8c010ece9f9d78
John Youssef <compujohnny@gmail.com>
2012-05-01 17:20:06 +0200

    Added Functionality

---
+++ theme/commerce-funds-account-balance.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = user_load($variables['uid']);
+
+$balance = db_query("SELECT * FROM {commerce_funds_user_funds} WHERE uid='$user->uid'")->fetchAssoc();
+
+if($balance){ ?>
+    
+<h3>Balance: </h3><span><?php print commerce_currency_format($balance['balance'], commerce_default_currency()); ?></span>
+
+<?php } else{ ?>
+
+<h3>Balance: </h3><span><?php print commerce_currency_format(0, commerce_default_currency()); ?></span>
+
+<?php } ?>
